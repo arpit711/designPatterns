b0f8a250f687df0c11109190392fdad45ff32e09
arpit711 <sharmaarpit291@gmail.com>
2024-01-28 18:10:11 +0530

    ObserverPatternDemo

---
+++ src/src/ObserverB.java
@@ -0,0 +1,10 @@
+public class ObserverB extends Observer{
+    public ObserverB(Subject subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+    @Override
+    public void update() {
+        System.out.println("String: " + Integer.toBinaryString(subject.getState()));
+    }
+}
---
+++ src/src/ObserverPatternDemo.java
@@ -0,0 +1,12 @@
+
+public class ObserverPatternDemo {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+        new ObserverA(subject);
+        new ObserverB(subject);
+        System.out.println("first state change 15");
+        subject.setState(15);
+        System.out.println("second state change 25");
+        subject.setState(25);
+    }
+}
